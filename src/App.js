4d5455e06f12597fc4584791b9b5235a90af09c0
Rashida Kamal <rsk2161@columbia.edu>
2020-04-18 21:45:23 -0400

    look ma, React! (with Grommet)

--- src/App.js
+++ src/App.js
@@ -1,60 +1,102 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { 
+	Box, 
+	Button, 
+	Collapsible, 
+	Heading, 
+	Grommet,
+	Layer,
+	ResponsiveContext
+ } from 'grommet'; 
+import { FormClose, Notification } from 'grommet-icons'; 
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      greeting: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6'
+    },
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
 
-  handleChange(event) {
-    this.setState({ name: event.target.value });
-  }
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small'}}
+    elevation='medium'
+    style={{ zIndex: '1'}}
+    {...props}
+  />
 
-  handleSubmit(event) {
-    event.preventDefault();
-    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
-      .then(response => response.json())
-      .then(state => this.setState(state));
-  }
+);
 
-  render() {
+const App = () =>  {
+	const [showSidebar, setShowSidebar] = useState(false);
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <form onSubmit={this.handleSubmit}>
-            <label htmlFor="name">Enter your name: </label>
-            <input
-              id="name"
-              type="text"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-            <button type="submit">Submit</button>
-          </form>
-          <p>{this.state.greeting}</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Grommet theme={theme} full>
+		<ResponsiveContext.Consumer>
+
+		{size => (
+          <Box fill>
+
+              <AppBar>
+                  <Heading level='3' margin='none'>My library app....</Heading>
+                  <Button icon={<Notification/>} onClick={() => setShowSidebar(!showSidebar)} />
+              </AppBar>
+              <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+
+                  	<Box flex align='center' justify='center'>app body</Box>
+
+					{(!showSidebar || size !== 'small') ? (
+					<Collapsible direction="horizontal" open={showSidebar}>
+						<Box
+							flex
+							width='medium'
+							background='light-2'
+							elevation='small'
+							align='center'
+							justify='center'>
+								sidebar
+						</Box>
+					</Collapsible>
+					): (
+						<Layer>
+
+							<Box
+								background='light-2'
+								tag='header'
+								justify='end'
+								align='center'
+								direction='row'>
+
+									<Button 
+										icon={<FormClose />} 
+										onClick={() => setShowSidebar(false)}/>
+							</Box>
+							<Box
+								fill
+								background='light-2'
+								align='center'
+								justify='center'>
+								sidebar
+							</Box>
+							
+						</Layer>
+					)}
+              </Box>
+          </Box>
+		)}
+		</ResponsiveContext.Consumer>
+        </Grommet>
     );
-  }
 }
 
 export default App;

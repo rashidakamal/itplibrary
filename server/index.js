142c97307c9f7162fcae3da2925fa0c1ab356831
Rashida Kamal <rsk2161@columbia.edu>
2020-04-23 17:29:29 -0400

    more

--- server/index.js
+++ server/index.js
@@ -19,6 +19,6 @@ app.get('/api/greeting', (req, res) => {
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.listen(3000, () =>
-  console.log('Express server is running on localhost:3000')
+app.listen(3001, () =>
+  console.log('Express server is running on localhost:3001')
 );

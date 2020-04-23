40e69e6fb7923ff8db9dc528b7e6989b1de3949d
Rashida Kamal <rsk2161@columbia.edu>
2020-04-23 17:24:13 -0400

    port changes

--- server/index.js
+++ server/index.js
@@ -6,12 +6,19 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
 
+console.log("I'm in the server.js code."); 
+
+app.get('/', (req, res) => {
+  
+  res.end("Hello library nerds....");
+});
+
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.listen(3001, () =>
+app.listen(3000, () =>
   console.log('Express server is running on localhost:3000')
 );

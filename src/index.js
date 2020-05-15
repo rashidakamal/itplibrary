2be1d78e91f836eff7c25231938915133e9808a4
Rashida Kamal <rsk2161@columbia.edu>
2020-05-14 21:02:49 -0400

    add billions of local changes to server

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import netlifyIdentity from 'netlify-identity-widget';
 window.netlifyIdentity = netlifyIdentity;
 netlifyIdentity.init();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
